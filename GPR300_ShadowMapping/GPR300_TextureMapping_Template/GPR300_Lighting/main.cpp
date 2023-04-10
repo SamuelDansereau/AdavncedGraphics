3a837236be9655d13438c9f4a1daf132ce3c8b43
Sam Dansereau <samuel.dansereau@mymail.champlain.edu>
2023-04-10 02:04:55 -0400

    I added back the directional light but I need to fix it I think

---
+++ GPR300_ShadowMapping/GPR300_TextureMapping_Template/GPR300_Lighting/main.cpp
@@ -0,0 +1,471 @@
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+#include <glm/glm.hpp>
+#include <glm/matrix.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <stdio.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
+#include "EW/Shader.h"
+#include "EW/EwMath.h"
+#include "EW/Camera.h"
+#include "EW/Mesh.h"
+#include "EW/Transform.h"
+#include "EW/ShapeGen.h"
+
+void processInput(GLFWwindow* window);
+void resizeFrameBufferCallback(GLFWwindow* window, int width, int height);
+void keyboardCallback(GLFWwindow* window, int keycode, int scancode, int action, int mods);
+void mouseScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+void mousePosCallback(GLFWwindow* window, double xpos, double ypos);
+void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
+
+float lastFrameTime;
+float deltaTime;
+
+int SCREEN_WIDTH = 1080;
+int SCREEN_HEIGHT = 720;
+
+double prevMouseX;
+double prevMouseY;
+bool firstMouseInput = false;
+
+/* Button to lock / unlock mouse
+* 1 = right, 2 = middle
+* Mouse will start locked. Unlock it to use UI
+* */
+const int MOUSE_TOGGLE_BUTTON = 1;
+const float MOUSE_SENSITIVITY = 0.1f;
+const float CAMERA_MOVE_SPEED = 5.0f;
+const float CAMERA_ZOOM_SPEED = 3.0f;
+
+Camera camera((float)SCREEN_WIDTH / (float)SCREEN_HEIGHT);
+
+glm::vec3 bgColor = glm::vec3(0);
+glm::vec3 lightColor = glm::vec3(1.0f);
+glm::vec3 lightPosition = glm::vec3(0.0f, 3.0f, 0.0f);
+
+bool wireFrame = false;
+
+const char* texture1File = "DiamondPlane.png";
+const char* texture1FileNorm = "DiamondPlateNormal.png";
+const char* texture2File = "Terrazzo.png";
+
+
+int activeT = 1;
+float speed = 1;
+
+struct DirLight {
+	glm::vec3 dir = glm::vec3(0, 0, 1);
+	glm::vec3 color = glm::vec3(1, 1, 1);
+	float intensity = 1.0;
+};
+
+struct PointLight {
+	glm::vec3 pos = glm::vec3(1);
+	glm::vec3 color = glm::vec3(1, 1, 1);
+	float intensity = 1;
+	float radius = 1;
+};
+
+struct Material
+{
+	glm::vec3 Color = glm::vec3(1, 1, 1);
+	float ambientK = 1, diffuseK = 1, specularK = 1;
+	float shininess = 1;
+};
+
+
+DirLight dlight;
+PointLight plight;
+Material mat;
+bool postProc = false;
+int activeEffect = 0;
+const char* effectNames[5] =
+{ "GreyScale", "Invert", "Wave X", "Wave Y", "Wave Both" };
+
+GLuint createTexture(const char* filePath)
+{
+	GLuint texture;
+	glGenTextures(1, &texture);
+
+	glBindTexture(GL_TEXTURE_2D, texture);
+	stbi_set_flip_vertically_on_load(true);
+	int width, height, numComponents;
+	unsigned char* textureData = stbi_load(filePath, &width, &height, &numComponents, 0);
+
+	switch (numComponents)
+	{
+	case 1:
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_R, width, height, 0, GL_R, GL_UNSIGNED_BYTE, textureData);
+		break;
+	case 2:
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RG, width, height, 0, GL_RG, GL_UNSIGNED_BYTE, textureData);
+		break;
+	case 3:
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, textureData);
+		break;
+	case 4:
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData);
+		break;
+	default:
+		break;
+	}
+
+	glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+
+	glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	return texture;
+}
+float normIntensity;
+
+int main() {
+	if (!glfwInit()) {
+		printf("glfw failed to init");
+		return 1;
+	}
+
+
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Lighting", 0, 0);
+	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK) {
+		printf("glew failed to init");
+		return 1;
+	}
+
+	glfwSetFramebufferSizeCallback(window, resizeFrameBufferCallback);
+	glfwSetKeyCallback(window, keyboardCallback);
+	glfwSetScrollCallback(window, mouseScrollCallback);
+	glfwSetCursorPosCallback(window, mousePosCallback);
+	glfwSetMouseButtonCallback(window, mouseButtonCallback);
+
+	//Hide cursor
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	// Setup UI Platform/Renderer backends
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init();
+
+	//Dark UI theme.
+	ImGui::StyleColorsDark();
+
+	GLuint tex1 = createTexture(texture1File);
+	GLuint tex2 = createTexture(texture2File);
+	GLuint tex1Norm = createTexture(texture1FileNorm);
+
+
+	//Used to draw shapes. This is the shader you will be completing.
+	Shader litShader("shaders/defaultLit.vert", "shaders/defaultLit.frag");
+
+	//Used to draw light sphere
+	Shader unlitShader("shaders/defaultLit.vert", "shaders/unlit.frag");
+
+	Shader framebuffer("shaders/framebuffer.vert", "shaders/framebuffer.frag");
+
+	ew::MeshData cubeMeshData;
+	ew::createCube(1.0f, 1.0f, 1.0f, cubeMeshData);
+	ew::MeshData sphereMeshData;
+	ew::createSphere(0.5f, 64, sphereMeshData);
+	ew::MeshData cylinderMeshData;
+	ew::createCylinder(1.0f, 0.5f, 64, cylinderMeshData);
+	ew::MeshData planeMeshData;
+	ew::createPlane(1.0f, 1.0f, planeMeshData);
+
+	ew::Mesh cubeMesh(&cubeMeshData);
+	ew::Mesh sphereMesh(&sphereMeshData);
+	ew::Mesh planeMesh(&planeMeshData);
+	ew::Mesh cylinderMesh(&cylinderMeshData);
+
+	ew::MeshData quadMeshData;
+	ew::createQuad(2.0f, 2.0f, quadMeshData);
+	ew::Mesh quadMesh(&quadMeshData);
+
+	//Enable back face culling
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+
+	//Enable blending
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	//Enable depth testing
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+
+	//Initialize shape transforms
+	ew::Transform cubeTransform;
+	ew::Transform sphereTransform;
+	ew::Transform planeTransform;
+	ew::Transform cylinderTransform;
+	ew::Transform lightTransform;
+
+	cubeTransform.position = glm::vec3(-2.0f, 0.0f, 0.0f);
+	sphereTransform.position = glm::vec3(0.0f, 0.0f, 0.0f);
+
+	planeTransform.position = glm::vec3(0.0f, -1.0f, 0.0f);
+	planeTransform.scale = glm::vec3(10.0f);
+
+	cylinderTransform.position = glm::vec3(2.0f, 0.0f, 0.0f);
+
+	lightTransform.scale = glm::vec3(0.5f);
+	lightTransform.position = glm::vec3(0.0f, 5.0f, 0.0f);
+
+	unsigned int fbo;
+	glGenFramebuffers(1, &fbo);
+	glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glActiveTexture(GL_TEXTURE2);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, SCREEN_WIDTH, SCREEN_HEIGHT, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture, 0);
+
+	unsigned int rbo;
+	glGenRenderbuffers(1, &rbo);
+	glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, SCREEN_WIDTH, SCREEN_HEIGHT);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+
+
+	while (!glfwWindowShouldClose(window)) {
+
+		processInput(window);
+		glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+		glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+		glClearColor(bgColor.r, bgColor.g, bgColor.b, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glEnable(GL_DEPTH_TEST);
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		float time = (float)glfwGetTime();
+		deltaTime = time - lastFrameTime;
+		lastFrameTime = time;
+
+		//Draw
+		litShader.use();
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, tex1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, tex1Norm);
+		litShader.setInt("activeTexture", 0);
+		litShader.setInt("normalMap", 1);
+		litShader.setFloat("normalIntensity", normIntensity);
+
+
+
+		litShader.setMat4("_Projection", camera.getProjectionMatrix());
+		litShader.setMat4("_View", camera.getViewMatrix());
+		litShader.setVec3("_LightPos", lightTransform.position);
+
+		litShader.setVec3("camPos", camera.getPosition());
+
+		litShader.setVec3("_Material.Color", mat.Color);
+		litShader.setFloat("_Material.ambinetK", mat.ambientK);
+		litShader.setFloat("_Material.diffuseK", mat.diffuseK);
+		litShader.setFloat("_Material.specularK", mat.specularK);
+		litShader.setFloat("_Material.shininess", mat.shininess);
+
+		litShader.setVec3("_dLight.dir", dlight.dir);
+		litShader.setVec3("_dLight.color", dlight.color);
+		litShader.setFloat("_dLight.intensity", dlight.intensity);
+
+		litShader.setVec3("_pLight.pos", plight.pos);
+		litShader.setVec3("_pLight.color", plight.color);
+		litShader.setFloat("_pLight.intensity", plight.intensity);
+		litShader.setFloat("_pLight.radius", plight.radius);
+
+
+		//Draw cube
+		litShader.setMat4("_Model", cubeTransform.getModelMatrix());
+		cubeMesh.draw();
+
+		//Draw sphere
+		litShader.setMat4("_Model", sphereTransform.getModelMatrix());
+		sphereMesh.draw();
+
+		//Draw cylinder
+		litShader.setMat4("_Model", cylinderTransform.getModelMatrix());
+		cylinderMesh.draw();
+
+		//Draw plane
+		litShader.setMat4("_Model", planeTransform.getModelMatrix());
+		planeMesh.draw();
+		litShader.setFloat("_Time", time);
+		litShader.setFloat("_Speed", speed);
+
+
+		//Draw light as a small sphere using unlit shader, ironically.
+		unlitShader.use();
+		unlitShader.setMat4("_Projection", camera.getProjectionMatrix());
+		unlitShader.setMat4("_View", camera.getViewMatrix());
+		unlitShader.setMat4("_Model", lightTransform.getModelMatrix());
+		unlitShader.setVec3("_Color", lightColor);
+		sphereMesh.draw();
+
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		glDisable(GL_DEPTH_TEST);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		framebuffer.use();
+		framebuffer.setInt("_post", int(postProc));
+		framebuffer.setInt("_effect", activeEffect);
+		framebuffer.setInt("_texture", 2);
+		framebuffer.setFloat("_time", time);
+		quadMesh.draw();
+
+
+
+		//Draw UI
+		ImGui::Begin("Settings");
+
+		ImGui::ColorEdit3("Material Color", &mat.Color.r);
+		ImGui::SliderFloat("Ambient", &mat.ambientK, 0, 1);
+		ImGui::SliderFloat("Diffuse", &mat.diffuseK, 0, 1);
+		ImGui::SliderFloat("Specular", &mat.specularK, 0, 1);
+		ImGui::SliderFloat("Shininess", &mat.shininess, 1, 512);
+		ImGui::ColorEdit3("Light Color", &dlight.color.r);
+		ImGui::DragFloat3("Light Direction", &dlight.dir.x);
+		ImGui::SliderFloat("Light Intensity", &dlight.intensity, 0, 1);
+		ImGui::SliderFloat("Normal Intensity", &normIntensity, 0, 1);
+
+		//ImGui::DragFloat("Speed", &speed, .25  , -5, 5);
+		ImGui::End();
+
+		ImGui::Begin("Post Processing");
+		ImGui::Checkbox("Post Processing on", &postProc);
+		ImGui::Combo("Effect", &activeEffect, effectNames, IM_ARRAYSIZE(effectNames));
+		ImGui::End();
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		glfwPollEvents();
+
+
+
+		glfwSwapBuffers(window);
+	}
+
+	glDeleteFramebuffers(1, &fbo);
+	glDeleteTextures(1, &texture);
+
+	glfwTerminate();
+	return 0;
+}
+//Author: Eric Winebrenner
+void resizeFrameBufferCallback(GLFWwindow* window, int width, int height)
+{
+	SCREEN_WIDTH = width;
+	SCREEN_HEIGHT = height;
+	camera.setAspectRatio((float)SCREEN_WIDTH / SCREEN_HEIGHT);
+	glViewport(0, 0, width, height);
+}
+//Author: Eric Winebrenner
+void keyboardCallback(GLFWwindow* window, int keycode, int scancode, int action, int mods)
+{
+	if (keycode == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+	//Reset camera
+	if (keycode == GLFW_KEY_R && action == GLFW_PRESS) {
+		camera.setPosition(glm::vec3(0, 0, 5));
+		camera.setYaw(-90.0f);
+		camera.setPitch(0.0f);
+		firstMouseInput = false;
+	}
+	if (keycode == GLFW_KEY_1 && action == GLFW_PRESS) {
+		wireFrame = !wireFrame;
+		glPolygonMode(GL_FRONT_AND_BACK, wireFrame ? GL_LINE : GL_FILL);
+	}
+}
+//Author: Eric Winebrenner
+void mouseScrollCallback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	if (abs(yoffset) > 0) {
+		float fov = camera.getFov() - (float)yoffset * CAMERA_ZOOM_SPEED;
+		camera.setFov(fov);
+	}
+}
+//Author: Eric Winebrenner
+void mousePosCallback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (glfwGetInputMode(window, GLFW_CURSOR) != GLFW_CURSOR_DISABLED) {
+		return;
+	}
+	if (!firstMouseInput) {
+		prevMouseX = xpos;
+		prevMouseY = ypos;
+		firstMouseInput = true;
+	}
+	float yaw = camera.getYaw() + (float)(xpos - prevMouseX) * MOUSE_SENSITIVITY;
+	camera.setYaw(yaw);
+	float pitch = camera.getPitch() - (float)(ypos - prevMouseY) * MOUSE_SENSITIVITY;
+	pitch = glm::clamp(pitch, -89.9f, 89.9f);
+	camera.setPitch(pitch);
+	prevMouseX = xpos;
+	prevMouseY = ypos;
+}
+//Author: Eric Winebrenner
+void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
+{
+	//Toggle cursor lock
+	if (button == MOUSE_TOGGLE_BUTTON && action == GLFW_PRESS) {
+		int inputMode = glfwGetInputMode(window, GLFW_CURSOR) == GLFW_CURSOR_DISABLED ? GLFW_CURSOR_NORMAL : GLFW_CURSOR_DISABLED;
+		glfwSetInputMode(window, GLFW_CURSOR, inputMode);
+		glfwGetCursorPos(window, &prevMouseX, &prevMouseY);
+	}
+}
+
+//Author: Eric Winebrenner
+//Returns -1, 0, or 1 depending on keys held
+float getAxis(GLFWwindow* window, int positiveKey, int negativeKey) {
+	float axis = 0.0f;
+	if (glfwGetKey(window, positiveKey)) {
+		axis++;
+	}
+	if (glfwGetKey(window, negativeKey)) {
+		axis--;
+	}
+	return axis;
+}
+
+//Author: Eric Winebrenner
+//Get input every frame
+void processInput(GLFWwindow* window) {
+
+	float moveAmnt = CAMERA_MOVE_SPEED * deltaTime;
+
+	//Get camera vectors
+	glm::vec3 forward = camera.getForward();
+	glm::vec3 right = glm::normalize(glm::cross(forward, glm::vec3(0, 1, 0)));
+	glm::vec3 up = glm::normalize(glm::cross(forward, right));
+
+	glm::vec3 position = camera.getPosition();
+	position += forward * getAxis(window, GLFW_KEY_W, GLFW_KEY_S) * moveAmnt;
+	position += right * getAxis(window, GLFW_KEY_D, GLFW_KEY_A) * moveAmnt;
+	position += up * getAxis(window, GLFW_KEY_Q, GLFW_KEY_E) * moveAmnt;
+	camera.setPosition(position);
+}
